f7a0d94ddfd060312a35f7a165986f07159ffbf2
ismaksu <mcpebagimli@gmail.com>
2023-06-25 15:28:20 +0300

    Automatic CreationDate and UpdateDate modifications.

--- NLayer.Repository/AppDbContext.cs
+++ NLayer.Repository/AppDbContext.cs
@@ -21,6 +21,49 @@ namespace NLayer.Repository
         public DbSet<Product> Products { get; set; }
         public DbSet<ProductFeature> ProductFeatures { get; set; }
 
+        public override int SaveChanges()
+        {
+            foreach (var item in ChangeTracker.Entries())
+            {
+                if (item.Entity is BaseEntity entityReference)
+                {
+                    switch (item.State)
+                    {
+                        case EntityState.Added:
+                            entityReference.CreationDate = DateTime.Now;
+                            break;
+                        case EntityState.Modified:
+                            Entry(entityReference).Property(x => x.CreationDate).IsModified = false;
+                            entityReference.UpdateDate = DateTime.Now;
+                            break;
+                    }
+                }
+            }
+            return base.SaveChanges();
+        }
+
+        public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+        {
+            foreach (var item in ChangeTracker.Entries())
+            {
+                if (item.Entity is BaseEntity entityReference)
+                {
+                    switch (item.State)
+                    {
+                        case EntityState.Added:
+                            entityReference.CreationDate = DateTime.Now;
+                            break;
+                        case EntityState.Modified:
+                            Entry(entityReference).Property(x => x.CreationDate).IsModified = false;
+                            entityReference.UpdateDate = DateTime.Now;
+                            break;
+                    }
+                }
+            }
+
+            return base.SaveChangesAsync(cancellationToken);
+        }
+
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());

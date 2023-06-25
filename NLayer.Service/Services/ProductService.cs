d3a11f689646ffd654acf4327e5bcf3fe0f7f8e4
ismaksu <mcpebagimli@gmail.com>
2023-06-25 18:18:20 +0300

    Reverted changes on structure for it to work on API properly.

--- NLayer.Service/Services/ProductService.cs
+++ NLayer.Service/Services/ProductService.cs
@@ -23,11 +23,11 @@ namespace NLayer.Service.Services
             _prodRepository = prodRepository;
         }
 
-        public async Task<List<ProductWithCategoryDto>> GetProductsWithCategory()
+        public async Task<CustomResponseDto<List<ProductWithCategoryDto>>> GetProductsWithCategory()
         {
             var products = await _prodRepository.GetProductsWithCategory();
             var productsDto = _mapper.Map<List<ProductWithCategoryDto>>(products);
-            return productsDto;
+            return CustomResponseDto<List<ProductWithCategoryDto>>.Success(200, productsDto);
         }
     }
 }
